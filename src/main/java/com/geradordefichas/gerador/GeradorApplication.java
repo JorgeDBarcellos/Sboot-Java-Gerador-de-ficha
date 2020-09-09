aaadcd1d841cb54d076236fb47aef1c8db33466c
Ericson.Bizarro <ericsonbizarro@gmail.com>
2020-09-09 00:09:39 -0300

    Primeiro Commit

---
+++ src/main/java/com/geradordefichas/gerador/GeradorApplication.java
@@ -0,0 +1,13 @@
+package com.geradordefichas.gerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorApplication.class, args);
+	}
+
+}
